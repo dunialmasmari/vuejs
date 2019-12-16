d6afaf275f2c327e488a716a38859a3430c64e02
Then Me <37501477+Thenmee@users.noreply.github.com>
2019-12-16 10:33:06 +0300

    files

---
+++ app.js
@@ -0,0 +1,55 @@
+new Vue({
+    el:'#vue-app',
+    data:{
+        age:20,
+        a:0,
+        b:0
+
+    },
+    methods:{
+       
+    },
+    computed:{
+        addToA:function(){
+            return this.a+this.age;
+         },
+         addToB:function(inc){
+             this.b+this.age;
+         }
+    }
+    
+
+    /*
+    data:{
+        name:'',
+        age :'',
+        x:0,
+        y:0
+    },
+    Methods:{
+        add:function(inc){
+            this.age+=inc;
+        },
+        subtract:function(dec){
+            this.age-=dec;
+        },
+        updatexy:function(event){
+            this.x=event.offsetX;
+            this.y=event.offsetY;
+
+        },
+        click:function(){
+            alert('You clicked me');
+
+        },
+        logName:function(){
+           // console.log('you entered your name');
+
+
+        },
+        logAge:function(){
+           // console.log('you entered your age');
+        }
+    }*/
+    });
+    
