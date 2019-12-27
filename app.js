e8e42a9cdce166fa59b1adaff4c251f211f345b4
Then Me <37501477+Thenmee@users.noreply.github.com>
2019-12-27 23:14:27 +0300

    simple punchbag

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 new Vue({
-    el:'#vue-app',
+    el: '#vue-app',
+    data: {
+        health: 100,
+        ended: false
+    },
+    methods: {
+        punch: function(){
+            this.health -= 10;
+            if ( this.health <= 0 ){
+                this.ended = true;
+            }
+        },
+        restart: function(){
+            this.health = 100;
+            this.ended = false;
+        }
+    },
+    computed: {
+
+    }
+});
+
+    /*
+  el:'#vue-app',
     data:{   
        output:"Your fav food"
     },
@@ -12,9 +35,6 @@ new Vue({
     computed:{
     
     }
-});
-
-    /*
 
     var data={
     name: 'Yushi'
